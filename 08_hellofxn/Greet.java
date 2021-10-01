ce9413e5159e1b6c1fe0801332f4344edd161e86
JosephOthman <jothman30@stuy.edu>
2021-10-01 12:44:52 -0400

    done

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,9 @@
+public class Greet {
+	public static void main (String[] args) {
+		System.out.println(greet(No hablo queso));
+
+	}
+	public static void greet(String x) {
+		System.out.println(x);
+	}
+}
