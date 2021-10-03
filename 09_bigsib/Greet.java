e9ffe088874a0151a2e56faf310be146f54f706a
joe othman <jothman30@stuy.edu>
2021-10-03 12:18:51 -0400

    done

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -5,17 +5,6 @@ public class Greet {
 		BigSib.greet("Kazaam");
 	}
 
-	public static String greet(String x) {
-		return x;
-	}
-
-	static String a = "Why, hello there, ";
-	static String b = ". How do you do?";
-	static String p = "John";
-	static String q = "Jacob";
-	static String r = "Josiah";
-	static String t = a + p + b;
-
 /*
 *DISCOVERIES
 */
