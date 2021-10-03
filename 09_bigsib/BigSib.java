707cfa382bd7a7ca28129135cba3781cf91e19a7
joe othman <jothman30@stuy.edu>
2021-10-03 12:17:55 -0400

    done?

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,13 @@
+public class BigSib {
+	public static void main (String[] args){
+		greet("Flim");
+	}
+
+	public static void greet(String x) {
+		System.out.println(a + x + b);
+	}
+
+	static String a = "Why, hello there, ";
+	static String b = ". How do you do?";
+
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,26 @@
+public class Greet {
+	public static void main (String[] args) {
+		BigSib.greet("Flim");
+		BigSib.greet("Flam");
+		BigSib.greet("Kazaam");
+	}
+
+	public static String greet(String x) {
+		return x;
+	}
+
+	static String a = "Why, hello there, ";
+	static String b = ". How do you do?";
+	static String p = "John";
+	static String q = "Jacob";
+	static String r = "Josiah";
+	static String t = a + p + b;
+
+/*
+*DISCOVERIES
+*/
+
+/*
+UNRESOLVED MYSTERIES
+*/
+}
