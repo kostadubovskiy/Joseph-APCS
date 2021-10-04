dad6f7249fa4e3e708caf6b1f099bbcb65cee1d8
joe othman <jothman30@stuy.edu>
2021-10-04 19:22:27 -0400

    done?

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,13 @@
+public class BigSib {
+	public static void main (String[] args){
+		greet("Flim");
+	}
+
+	public static String greet(String x) {
+		return a + x + b;
+	}
+
+	static String a = "Why, hello there, ";
+	static String b = ". How do you do?";
+
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,15 @@
+/*
+Joseph, Lindsay, Andrew, Po, Tommy, Fidget
+APCS
+HW10 -- BigSib use #2. Output of BigSib greet is String, not void here.
+2021-10-04
+*/
+
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}
+
