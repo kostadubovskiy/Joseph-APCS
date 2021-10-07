47d694b9d97e5f339a36894e229438450d72ec7c
joe othman <jothman30@stuy.edu>
2021-10-06 20:40:14 -0400

    done :D cd Joseph-APCS git push

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,8 +1,8 @@
 /*
 Joseph, Kosta, Po, Davie
 APCS
-HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
-2021-10-05
+HW13 -- Where do Big Sibs Come From?
+2021-10-06
 */
 
 public class BigSib {
@@ -32,9 +32,12 @@ public class BigSib {
 
 /*
 DISCO
-- You can set a variable, and then use a function to set the value of that variable.
-- You can use the BigSib richard = new BigSib(); syntax to run BigSib functions with different inputs as much as you want.
+- When we use the "BigSib richard = new BigSib()" syntax, we can acutally  give inputs to the instance of BigSib (called richard) within the parenthesis, which the constructor will use in its code.
+- We postulate that the constructor needs the same name as the class because when we use the new instance syntax, we write new BigSib(). possibly with parameters. The constructor must be named BigSib or else it will not be run.
+
 QCC
-- What allows a function to set the value of a variable that isn't an input of the function?
-- Why would we use this syntax instead of other types?
+- What happens if there is more than one input? Should they be ordered a certain way?
+- Why use constructors to initialize variables? Does it provide a functional use that we haven't had before?
+- Can the constructor initiate more than one variable? What is the syntax, if it can?
+- Can you have more than one constructor in a class?
 */
