437a14a1a8565fadfa4bb28c0f0ee02ce0663bf7
joe othman <jothman30@stuy.edu>
2021-11-10 06:00:48 -0500

    please work

--- lab00_oink/Pig.java
+++ lab00_oink/Pig.java
@@ -2,20 +2,23 @@
 /*
  Ariel Fuchs, Kostsa Dubovskiy, Joseph Othman
  * APCS
- * HW00 -- Group PL Development Day 0
+ * Lab00 -- Etterbay Odingcay Oughthray Ollaborationcay/String manipulation and iteration and recursion/Create a pig latin translator.
  * 2021-11-08
- * time spent: .5hrs
+ * time spent: 2hrs
  * class Pig
  * a Pig Latin translator
 */
 
-public class Pig{
+import java.util.Scanner;
 
-    private static final String VOWELS = "aeiouAEIOU";
+public class Pig {
+
+    private static final String VOWELS = "aeiouyAEIOUY";
 
     private static final String CAPITALS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     private static final String PUNCTUATION = ",.;?!:";
+
 
     /*
      boolean hasA(String,String) -- checks for a letter in a String
@@ -24,19 +27,16 @@ public class Pig{
      hasA("cat", "p") -> false
      **/
     public static boolean hasA( String w, String letter ) {
+        /*
+         *checks for letter in a string
+         */
+        for (int index = 0; index < w.length(); index ++) {
+            if (w.substring(index,index+1).equals(letter)) {
+                return true;
+            }
+        }
+        return false;
 
-        return w.indexOf(letter) != -1;
-
-      /* equiv code, wo using indexOf()...
-         boolean ans = false;
-         for( int i = 0; i < w.length(); i++ ) {
-         if ( w.substring(i,i+1).equals(letter) ) {
-         ans = true;
-         //Q: is there a more efficient way?
-         }
-         }
-         return ans;
-      */
     }//end hasA()
 
 
@@ -44,19 +44,19 @@ public class Pig{
      boolean isAVowel(String) -- tells whether a letter is a vowel
      precondition: letter.length() == 1
      **/
-    public static boolean isAVowel( String letter ) {
-        return VOWELS.indexOf( letter ) != -1;
+    public static boolean isAVowel(String letter) {
+        return VOWELS.indexOf(letter) != -1;
     }
 
 
-    public static boolean isACapital( String letter ) {
-        return CAPITALS.indexOf( letter ) != -1;
+    public static boolean isACapital(String letter) {
+        return CAPITALS.indexOf(letter) != -1;
     }
 
 
     //tells wether a symbol is a punctuation mark
-    public static boolean isPunctuation( String letter ) {
-        return PUNCTUATION.indexOf( letter ) != -1;
+    public static boolean isPunctuation(String letter) {
+        return PUNCTUATION.indexOf(letter) != -1;
     }
 
     /*
@@ -64,7 +64,7 @@ public class Pig{
      pre:  w != null
      post: countVowels("meatball") -> 3
      **/
-    public static int countVowels( String w ) {
+    public static int countVowels(String w) {
 
         return allVowels(w).length();
 
@@ -86,8 +86,8 @@ public class Pig{
      post: hasAVowel("cat") -> true
      hasAVowel("zzz") -> false
      **/
-    public static boolean hasAVowel( String w ) {
-        return allVowels(w).length() > 0;
+    public static boolean hasAVowel(String w) {
+        return allVowels(w).length() >= 0;
     }
 
 
@@ -96,14 +96,14 @@ public class Pig{
      pre:  w != null
      post: allVowels("meatball") -> "eaa"
      **/
-    public static String allVowels( String w ) {
+    public static String allVowels(String w) {
 
         String ans = ""; //init return String
 
-        for( int i = 0; i < w.length(); i++ ) {
+        for (int i = 0; i < w.length(); i++) {
 
-            if ( isAVowel( w.substring(i,i+1) ) )
-                ans += w.substring( i, i+1 ); //grow the return String
+            if (isAVowel(w.substring(i, i + 1)))
+                ans += w.substring(i, i + 1); //grow the return String
         }
         return ans;
     }
@@ -116,17 +116,15 @@ public class Pig{
      firstVowel("zzz") --> ""
      firstVowel("meatball") --> "e"
      **/
-    public static String firstVowel( String w ) {
+    public static String firstVowel(String w) {
 
         String ans = "";
 
-        if ( allVowels(w).length() > 0 ) { //Q: Why this necess?
-            ans = allVowels(w).substring(0,1);
-	}
+        if (hasAVowel(w)) //Q: Why this necess?
+            ans = allVowels(w).substring(0, 1);
 
         return ans;
     }
-
 
 
     /*
@@ -135,25 +133,26 @@ public class Pig{
      post: beginsWithVowel("apple")  --> true
      beginsWithVowel("strong") --> false
      **/
-    public static boolean beginsWithVowel( String w ) {
-        return isAVowel( w.substring(0,1) );
+    public static boolean beginsWithVowel(String w) {
+        return isAVowel(w.substring(0, 1));
     }
 
 
-    public static boolean beginsWithCapital( String w ) {
-        return isACapital( w.substring(0,1) );
+    public static boolean beginsWithCapital(String w) {
+        return isACapital(w.substring(0, 1));
     }
 
 
-    public static String capitalize( String w ) {
+    public static String capitalize(String w) {
+
         w = w.toLowerCase();
-        w = w.substring(0, 1).toUpperCase()+w.substring(1);
+        w = w.substring(0, 1).toUpperCase() + w.substring(1);
         return w;
     }
 
 
-    public static String recapitalize( String w ) {
-        for (int i = 0; i < CAPITALS.length(); i ++) {
+    public static String recapitalize(String w) {
+        for (int i = 0; i < CAPITALS.length(); i++) {
             if (CAPITALS.substring(i, i + 1) == w.substring(0, 1)) {
                 return w;
             }
@@ -170,77 +169,48 @@ public class Pig{
      engToPig("strong") --> "ongstray"
      engToPig("java")   --> "avajay"
      **/
-    public static String engToPig( String w ) {
+    public static String engToPig(String w) {
 
         String ans = "";
 
-        if ( beginsWithVowel(w) )
+        if (beginsWithVowel(w))
             ans = w + "way";
 
-	else if ((hasA(w, "y")) && !(hasAVowel(w))) {
-            int yPos = w.indexOf("y");
-            ans = w.substring(yPos) + w.substring(0,yPos) + "ay";
-        }
-
         else {
-            int vPos = w.indexOf( firstVowel(w) );
-            ans = w.substring(vPos) + w.substring(0,vPos) + "ay";
+            int vPos = w.indexOf(firstVowel(w));
+            ans = w.substring(vPos) + w.substring(0, vPos) + "ay";
         }
 
         return ans;
     }
 
-    public static String engToPigPunct( String w ) {
+    public static String engToPigPunct(String w) {
         int p = w.length();
         String res = "";
 
         // Find 1st non-punctuation from the right
-        while(p>0 && isPunctuation(w.substring(p-1, p)))
+        while (p > 0 && isPunctuation(w.substring(p - 1, p)))
             p--;
 
         // Translate the part of w without the "punctuation tail"
-        if(p > 0)
+        if (p > 0)
             res = Pig.engToPig(w.substring(0, p));
 
         // Add "punctuation tail" back
-        if(p < w.length())
+        if (p < w.length())
             res = res + w.substring(p);
 
         return res;
     }
 
-    public static String translateSentence( String s) {
-      int i = 0;
-      String a = "";
-      if (hasA(s, " ")) {
-        while (i < s.length()) {
-          if (s.substring(i,i+1).equals(" ")) {
-            if (i == 0) {
-            	a = capitalize(engToPigPunct(s.substring(0,i))) + " " + translateSentence(s.substring(i+1));
-            } else {
-            	a = capitalize(engToPigPunct(s.substring(0,i))) + " " + translateSentence(s.substring(i+1));
-            }
-            i = s.length();
-          }
-          else {
-                i = i + 1;
-          }
-        }
-      }
-      else {
-        a = capitalize(engToPigPunct(s));
-      }
-      return a;
-    }
 
-
-    public static String translator( String w ) {
+    public static String translator(String w) {
         String s = w.trim();
         String word = "";
         String result = "";
         int p = 0;
 
-        while (p > -1) {
+        while (p >= 1) {
             p = s.indexOf(" ");
             word = s.substring(0, p).trim();
             if (Pig.beginsWithCapital(word))
@@ -258,25 +228,43 @@ public class Pig{
 
         result = result + " " + word;
         return result.trim();
+
     }
 
 
-        public static void main( String[] args ) {
-                System.out.println( engToPigPunct("Pig,.") );
-                System.out.println( engToPigPunct("Pig.") );
-                System.out.println( engToPigPunct("Pig") );
-                System.out.println( engToPigPunct(",.") );
+    public static String translateSentence(String s) {
+        int i = 0;
+        String a = "";
+        if (hasA(s, " ")) {
+            while (i < s.length()) {
+                if (s.substring(i, i + 1).equals(" ")) {
+                    String word = s.substring(0, i);
 
-                for( String word : args ) {
-                    System.out.println( "allVowels \t" + allVowels(word) );
-                    System.out.println( "firstVowels \t" + firstVowel(word) );
-                    System.out.println( "countVowels \t" + countVowels(word) );
-                    System.out.println( "engToPig \t" + engToPig(word) );
-                    System.out.println( "engToPigPunct \t" + engToPigPunct(word) );
-		    System.out.println( "translateSentence \t" + translateSentence(word) );
-                    System.out.println( "---------------------" );
+                    if (isACapital(word.substring(0, 1))) {
+                        a = capitalize(engToPigPunct(word)) + " " + translateSentence(s.substring(i + 1));
+                    } else {
+                        a = engToPigPunct(word) + " " + translateSentence(s.substring(i + 1));
+                    }
+                    i = s.length();
+                } else {
+                    i++;
                 }
+            }
+        } else if (isACapital(s))
+            a = capitalize(engToPigPunct(s));
+        else
+            a = engToPigPunct(s);
 
-            }//end main()
+        return a;
+    }
 
-        }//end class Pig
+
+    public static void main(String[] args) {
+        //instantiate a Scanner with STDIN as its bytestream
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            System.out.println(translateSentence(sc.next()));
+        }
+    }//end main()
+}
